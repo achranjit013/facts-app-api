69d665e6527fe8d0f332be6c899395206bb1c381
Ranjit Acharya <achranjit013@gmail.com>
2023-11-27 00:56:25 +1100

    completed post and get methods for facts

--- src/model/facts/FactsModel.js
+++ src/model/facts/FactsModel.js
@@ -9,7 +9,14 @@ export const insertFact = (factObj) => {
 
 // read facts
 // if logged in: facts of logged user only
+export const getAllFactsByUserId = (userId) => {
+  return FactsSchema.find({ userId });
+};
+
 // if not logged in: all facts
+export const getAllFacts = () => {
+  return FactsSchema.find();
+};
 
 // update facts
 
--- src/router/factsRouter.js
+++ src/router/factsRouter.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { insertFact } from "../model/facts/FactsModel.js";
+import {
+  getAllFacts,
+  getAllFactsByUserId,
+  insertFact,
+} from "../model/facts/FactsModel.js";
 import { userAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -26,6 +30,48 @@ router.post("/", userAuth, async (req, res, next) => {
 });
 
 // read facts
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await getAllFacts();
+
+    result?.length > 0
+      ? res.json({
+          status: "success",
+          message:
+            "Congratulations!!! You are successfull in getting facts list.",
+          result,
+        })
+      : res.json({
+          status: "error",
+          message:
+            "Sorry, there was some problem while retriving the facts. Please try again later or contact admin.",
+        });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// read facts - for logged user
+router.get("/user-dashboard", userAuth, async (req, res, next) => {
+  try {
+    const result = await getAllFactsByUserId(req.userId);
+
+    result?.length > 0
+      ? res.json({
+          status: "success",
+          message:
+            "Congratulations!!! You are successfull in getting facts list.",
+          result,
+        })
+      : res.json({
+          status: "error",
+          message:
+            "Hi there, you haven't posted any facts yet. Please post a fact and get engaged with the world!",
+        });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // update facts
 
